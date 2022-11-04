128c1a836089153b7f018a23b8b738c9365da8da
tokkissi <alal4674@gmail.com>
2022-11-04 14:48:29 +0900

    [fix] : navbar 및 카테고리의 id 중복 사용건 수정

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -3,15 +3,20 @@
 // 코드 예시를 남겨 두었습니다.
 
 import * as Api from '../api.js';
-// import { randomId } from '/useful-functions.js';
+import {
+  sortByProductNo,
+  sortByLowerPrice,
+  sortByHigherPrice,
+} from '../useful-functions.js';
 
 // import { getCategories } from '../../controller/category-controller.js';
 
 try {
   // 응답으로 가져올 더미 데이터 예시
-  const datas = {
+  const itemlist = {
     items: [
       {
+        productNo: '00000023',
         name: '소세지',
         brand: 'kakao',
         price: '20000',
@@ -19,6 +24,7 @@ try {
         category: 0,
       },
       {
+        productNo: '00000052',
         name: '고등어',
         brand: 'kakao',
         price: '10000',
@@ -26,6 +32,7 @@ try {
         category: 1,
       },
       {
+        productNo: '00000035',
         name: '열라면',
         brand: 'kakao',
         price: '4000',
@@ -33,6 +40,7 @@ try {
         category: 2,
       },
       {
+        productNo: '00000019',
         name: '참치',
         brand: 'kakao',
         price: '30000',
@@ -42,18 +50,26 @@ try {
     ],
     message: 'success',
   };
+
+  // 상품 등록 번호를 이용, 최근등록순으로 정렬
+  let sortedByPNo = sortByProductNo(itemlist.items);
+
+  // 상품 카테고리 페이지에서 사용
+  // let sortedByLP = sortByLowerPrice(itemlist.items);
+  // let sortedByHP = sortByHigherPrice(itemlist.items);
+  console.log(sortedByPNo);
 } catch {
   console.error(err.stack);
   alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
 }
 
-async function addProductData() {
-  const datas = await Api.get('http://localhost:5000/items');
-  console.log(datas);
-}
-addProductData();
+// async function addProductData() {
+//   const datas = await Api.get('http://localhost:5000/items');
+//   console.log('datas', datas);
+// }
+// addProductData();
 
-console.log(Api);
+// console.log(Api);
 // console.log(getCategories());
 // async function getDataFromApi() {
 //   // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
--- src/views/useful-functions.js
+++ src/views/useful-functions.js
@@ -4,26 +4,73 @@ export const randomId = () => {
 };
 
 // 이메일 형식인지 확인 (true 혹은 false 반환)
-export const validateEmail = (email) => {
+export const validateEmail = email => {
   return String(email)
     .toLowerCase()
     .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     );
 };
 
 // 숫자에 쉼표를 추가함. (10000 -> 10,000)
-export const addCommas = (n) => {
-  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+export const addCommas = n => {
+  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 // 13,000원, 2개 등의 문자열에서 쉼표, 글자 등 제외 후 숫자만 뺴냄
 // 예시: 13,000원 -> 13000, 20,000개 -> 20000
-export const convertToNumber = (string) => {
-  return parseInt(string.replace(/(,|개|원)/g, ""));
+export const convertToNumber = string => {
+  return parseInt(string.replace(/(,|개|원)/g, ''));
 };
 
 // ms만큼 기다리게 함.
-export const wait = (ms) => {
-  return new Promise((r) => setTimeout(r, ms));
+export const wait = ms => {
+  return new Promise(r => setTimeout(r, ms));
+};
+
+// 상품 배열을 최근등록순으로 정렬해주는 함수
+export const sortByProductNo = arr =>
+  arr.sort((a, b) => Number(b.productNo) - Number(a.productNo));
+
+// 상품 배열을 낮은가격순으로 정렬해주는 함수
+export const sortByLowerPrice = arr =>
+  arr.sort((a, b) => Number(a.price) - Number(b.price));
+
+// 상품 배열을 높은가격순으로 정렬해주는 함수
+export const sortByHigherPrice = arr =>
+  arr.sort((a, b) => Number(b.price) - Number(a.price));
+
+// 상품 추가 무한스크롤 -- 수정 중 --
+export const infiniteScroll = () => {
+  const listEnd = document.querySelector('.product-container');
+  const itemList = document.querySelectorAll('.product-item');
+  const option = {
+    root: null,
+    rootMargin: '0px',
+    thredhold: '0',
+  };
+
+  const template = `
+  <li class="product-item" style="width:25%;">
+  <div class="item-container">
+  <div class="item-photobox"><a href="${productDetailUrl}"><img src="${productImgUrl}" alt="${productName}"></a></div>
+  <div class="item-info-container">
+  <div class="item-namebox"><strong><span>${productName}</span></strong></div>
+  <div class="item-pricebox"><strong><span>${productPrice}</span></strong></div>
+  </div>
+  </div>
+  </li>
+`;
+
+  const onIntersect = entries => {
+    entries.forEach(el => {
+      if (el.isIntersecting) {
+        const listWrap = document.querySelector('.product-ul');
+        listWrap.insertAdjacentHTML('beforeend', addedItems);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(onIntersect, option);
+  observer.observe(listEnd);
 };
