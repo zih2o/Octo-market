8a2724dc6d6f156a4196c2b250bd9d6b24f06312
Lamnet <semin0706@kaist.ac.kr>
2022-11-04 19:28:35 +0900

    [Feat] : Initial link to defined APIs done

--- src/views/myPage/delivery-admin.js
+++ src/views/myPage/delivery-admin.js
@@ -1,8 +1,71 @@
+import * as Api from '../api.js';
+
+const orderList = document.querySelector('#orderList');
+
+/* Data mocked
+    const order = [
+	{state:"배송 중",address:"B",items:[["C", 0]], createdAt:"221104"},
+	{state:"결제 완료",address:"F",items:[["G", 0]], createdAt:"221103"},
+]
+*/
+allOrdersAdmin()
+{/* <th>00002</th>
+<td>semin0706@naver.com</td>
+<td>22.11.03</td>
+<td></td>
+<td>나쵸 외 5개</td>
+<td>50000</td>
+<td>배송 중</td>
+<td><btn class="button is-small is-danger is-outlined">취소</btn></td> */}
 
 
-var orderTemplate = `<tr>
-<th>${orderID}</th>
-<td>${orderInfo}</td>
-<td>${deliveryStatus}</td>
-<td>삭제</td>
-</tr>`
+async function allOrdersAdmin()
+{
+    /* Login validation */
+    const token = sessionStorage.getItem("token");
+    if (!token){
+        alert("로그인 후 이용해 주세요.");
+        window.location.href = "/login";
+    } 
+    /* Admin validation is needed */
+    
+
+    /* Read and Add rows to table if get request is successful */
+    var retHtml = "";
+
+    try 
+    {
+        //const orders = await Api.get(`http://localhost:5050/admin/orders`);
+        const orders = [
+            {state:"배송 중",address:"B",items:[["C", 0]], createdAt:"221104"},
+            {state:"결제 완료",address:"F",items:[["G", 0]], createdAt:"221103"},
+        ]
+        var count = 1;
+        
+        for (let order of orders){
+            const {state, address, items, createdAt} = order;
+            let tableContent = `<tr><th>${String(count++).padStart(6, '0')}</th>`;
+            let userName = `<td></td>`;
+            let orderDate = `<td>${createdAt}</td>`
+            let userAddr = `<td>${address}</td>`
+            let orderName = `<td>${items[0][0]} 등 ${items.length}개</td>`;
+            let price = `<td></td>`;
+            let stateDef = `<td>${state}</td>`;
+            let buttons = `<td><btn class="button is-small is-danger is-outlined">취소</btn></td></tr>`;
+            retHtml += (tableContent + userName + orderDate + userAddr + orderName + price + stateDef + buttons);
+        }
+
+        orderList.insertAdjacentHTML("beforeend", retHtml);
+
+    }
+    catch(e)
+    {
+        console.error(e.stack);
+        alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${e.message}`);
+    }
+}
+
+function orderSum (inArr)
+{
+    console.log("not implemented")
+}
--- src/views/myPage/modifyAccount.js
+++ src/views/myPage/modifyAccount.js
@@ -1,9 +1,10 @@
+import * as Api from '../api.js';
+
 const emailIn = document.querySelector("#emailIn")
 const oldpwIn = document.querySelector("#oldpwIn")
 const newpwIn = document.querySelector("#newpwIn")
 const addrIn1 = document.querySelector("#sample6_address")
 const addrIn2 = document.querySelector("#sample6_detailAddress")
-
 const phone = document.querySelector("#phoneNum")
 const submitBtn = document.querySelector("#submitBtn")
 
@@ -40,17 +41,30 @@ submitBtn.addEventListener("click", async (e)=>{
         newpw,
         address,
         phoneNum,
+        type:"user",
     };
 
-    const dataJson = JSON.stringify(data);
-    const res = await fetch (`http://localhost:5000/users/updateinfo/${userId}`, 
+    try {
+        const res = await Api.patch(`http://localhost:5050/users/updateinfo/${userId}`, data);
+        if (res.status == 200) {
+            alert("수정되었습니다.");
+            return;
+        }
+        else if (res.status == 403)
+        {
+            alert("비밀번호가 일치하지 않습니다.")
+            return;
+        }
+        else if (res.status == 404)
+        {
+            alert("해당하는 유저가 없습니다.")
+            return;
+        }
+        window.location.href = window.location.href
+    }
+    catch (e)
     {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: dataJson,
-    })
-
-    
+        console.error(err.stack);
+        alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+    }
 })
