b3b2510353148169a17b3eda9057081236c2b7ad
Lamnet <semin0706@kaist.ac.kr>
2022-11-09 00:12:23 +0900

    [Feat] : File upload enabled

--- src/views/addItem/addItem.js
+++ src/views/addItem/addItem.js
@@ -1,4 +1,4 @@
-import * as API from '../api.js';
+import * as Api from '../api.js';
 
 //This page is rendered when URI is given as /admin/items
 
@@ -9,7 +9,8 @@ const prodDesc = document.querySelector('#productDesc');
 const prodSrc  = document.querySelector('#productSrc');
 const prodPrice= document.querySelector('#productPrice');
 const btnSubmit= document.querySelector('#btnSubmit');
-
+const filebtn  = document.querySelector(".file-input");
+const filename = document.querySelector(".file-name")
 // Data mocked
 // sessionStorage.setItem('loginToken', JSON.stringify({
 //     accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzY0OWI5ZDgwYTMzZTUwMWNlNWY5NDYiLCJyb2xlIjoidXNlciIsImlhdCI6MTY2NzUzODI5OX0.38U02nnJHS_UaEdR5weEll3wKzLE1zS-_f6FTIkdB10",
@@ -36,8 +37,12 @@ const btnSubmit= document.querySelector('#btnSubmit');
 // }
 
 
-const {accessToken, userId, userType} = JSON.parse(sessionStorage.getItem('loginToken'));
-const emailToken = sessionStorage.getItem('userEmail')
+const {accessToken, userId, userType} = {
+    "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzY4YmRmNzQ5ZGZlODA4OTg4ZWEyMTIiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2Njc4OTk2NTYsImV4cCI6MTY2Nzk4NjA1Nn0.A62VUIXHdHdJocM-ws40oEeq4ppj9N3sf6tfP33WWmI",
+    "userId": "6368bdf749dfe808988ea212",
+    "userType": "admin"
+}
+sessionStorage.setItem('token', accessToken)
 
 addAllElements()
 addAllEvents()
@@ -50,19 +55,42 @@ function addAllElements() {
 }
 function addAllEvents() {
     btnSubmit.addEventListener('click', addItemtoDB)
+    filebtn.addEventListener('change', fileHandler)
+}
+
+async function fileHandler(e) {
+    // console.log(e.target.files[0])
+    let formData = new FormData();
+    formData.append("image", e.target.files[0])
+
+    console.log(formData)
+    const res = await fetch('/admin/items/upload', {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+        body: formData,
+    })
+
+    const {imageUrl} = await res.json()
+    filename.innerHTML = imageUrl
+
+    
 }
 
 
+
 async function getCategory() {
-    // const category = resCate.categories;
-    const res = await API.get('/categories');
-    const category = res.categories;
-    if (res.status === 200){
+    try {
+        const category = await Api.get('/categories');
         for (let i=0; i<category.length;i++)
             prodCate.insertAdjacentHTML("beforeend", `<option>${category[i].name}</option>`)
-    }
-    else
-        return alert("카테고리 조회에 실패하였습니다")
+        }
+    catch (e)
+        {
+            console.error(err.stack);
+            alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+        }
 }
 
 
@@ -83,7 +111,7 @@ async function addItemtoDB () {
         category,
     }
     try {
-        const res = await API.post(`/admin/items`, data);
+        const res = await Api.post(`/admin/items`, data);
         // const res = JSON.stringify({
         //     status : 403, 
         //     body : {
