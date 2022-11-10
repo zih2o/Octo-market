5972873d4783cf8c71f75aa20612e6e95c2ece16
지윤 김 <jo522115@gmail.com>
2022-11-10 11:25:21 +0000

    [Fix]: url 수정

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -103,7 +103,7 @@ categoryList.then(datas => {
   // 카테고리 클릭 이벤트 발생 시 박스에 데이터를 채워주는 함수
   // 카테고리 명을 categoryId 로 받는다
   const fillRecommend = async (categoryId) => {
-    let url = '/items/category/'+categoryId;
+    let url = categoryId;
     let params = {
       re: 'isRecommend', // 추천상품인지 확인을 위한 쿼리
       isRecommend: true, // boolean 입력
