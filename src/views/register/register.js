e9a0993daf6340704972913c82f59f039879a6b8
지윤 김 <jo522115@gmail.com>
2022-11-10 08:57:16 +0000

    [Fix]: usefulfunction 적용

--- src/views/register/register.js
+++ src/views/register/register.js
@@ -8,12 +8,14 @@ import {
   activeNavbar,
   // 푸터 랜더링
   drawFooter,
+  drawAdminLink
 } from "../../useful-functions.js";
 
 // html 랜더링 관련 함수들 실행
 drawNavbar();
 activeNavbar();
 drawFooter();
+drawAdminLink();
 
 // 요소(element), input 혹은 상수
 const fullNameInput = document.querySelector("#fullNameInput");
