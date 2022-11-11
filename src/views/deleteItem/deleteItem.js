9e3bca4ab1e82c87f49c776f6f50146e91e463f9
Lamnet <semin0706@kaist.ac.kr>
2022-11-12 02:22:22 +0900

    [Feat] : item modify using modal

--- src/views/deleteItem/deleteItem.js
+++ src/views/deleteItem/deleteItem.js
@@ -16,6 +16,10 @@ import {
   drawFooter();
   drawAdminLink();
 
+const prodName = document.querySelector('#productName');
+const prodBrand= document.querySelector('#productBrand');
+const prodCate = document.querySelector('#productCategory');
+const prodDesc = document.querySelector('#productDesc');
 
 //This page is rendered when URI is given as /admin/orders/
 const orderList = document.querySelector('#orderList');
@@ -27,7 +31,13 @@ const userType = sessionStorage.getItem("adminToken")
 
 //const emailToken = sessionStorage.getItem('userEmail')
 
-var idArray = [];
+var itemArray = [];
+var descArray = [];
+var selectedItem;
+const categorylist = await (await fetch(`/categories`)).json()
+const modalCloseModal = document.querySelectorAll(".close-button");
+const modalPutModal = document.querySelector(".put-button");
+
 addAllElements();
 addAllEvents();
 
@@ -36,12 +46,15 @@ function addAllElements() {
     isLoggedIn();
     isAdmin();
     allOrdersAdmin();
+    getCategory();
 }
 
 function addAllEvents() {
     // Add row delete and call delivery cancel api
     orderList.addEventListener("click", e => Update(e))
     document.addEventListener("DOMContentLoaded", e => modalManipulate())
+    modalPutModal.addEventListener("click", e => updateData(e))
+    modalCloseModal.forEach(button => button.addEventListener("click", closeModal));
 }
 
 
@@ -53,7 +66,6 @@ async function allOrdersAdmin()
     try
     {
         const items = await (await fetch(`/items`)).json();
-        const categorylist = await (await fetch(`/categories`)).json()
 
 
         if (items.length > 0)
@@ -63,19 +75,19 @@ async function allOrdersAdmin()
         
         var count = 1;
         for (let item of items){
-            const {name, brand, price, createdAt, category, description, isRecommend, isDiscount, id} = item;
+            const {name, brand, price, createdAt, category, description, imageUrl, isRecommend, isDiscount, dispercent, id} = item;
             let tableContent = `<tr><th>${String(count++).padStart(5, '0')}</th>`;
-            idArray.push(id)
             let itemName = `<td>${name}</td>`;
             let brandName = `<td>${brand}</td>`;
-            let categoryName = `<td>${matchCategory(category, categorylist)}</td>`
+            let nameCate = matchCategory(category, categorylist)
+            let categoryName = `<td>${nameCate}</td>`
             let orderDate = `<td>${createdAt.split('T')[0].replaceAll('-', '.')}</td>`
             let itemprice = `<td>${price}</td>`;
-
             let buttons = `<td>
             <btn class="js-modal-trigger button is-small is-info is-outlined" id="modify" data-target="modal-js-example">수정</btn>
             <btn class="button is-small is-danger is-outlined" id="delete">삭제</btn></td></tr>`;
             retHtml += (tableContent + itemName + brandName + categoryName +  orderDate + itemprice + buttons);
+            itemArray.push({name, brand, price, description, category, imageUrl, isRecommend, isDiscount, dispercent, id})
         }
         orderList.insertAdjacentHTML('beforeend', retHtml)
     }
@@ -86,9 +98,40 @@ async function allOrdersAdmin()
     }
 }
 
+
+function populateForm(itemRow) {
+    selectedItem = itemArray[Number(itemRow.cells[0].innerHTML-1)];
+    prodName.setAttribute('value', selectedItem.name)
+    prodBrand.setAttribute('value', selectedItem.brand)
+    //prodCate.value = matchCategory(selectedItem.category, categorylist)
+    prodDesc.value = selectedItem.description
+    //delete selectedItem.id
+}
+
+async function updateData(e) {
+    console.log("control flow")
+    const itemId = selectedItem.id;
+    selectedItem.name = prodName.value
+    selectedItem.brand = prodBrand.value
+    selectedItem.category = deMatchCategory(prodCate.value, categorylist)
+    selectedItem.description = prodDesc.value
+    const {name, brand, price, description, imageUrl, isRecommend, isDiscount} = selectedItem
+
+    const postData = 
+    {
+        name, brand, price, description, imageUrl, isRecommend, isDiscount
+    }
+    const res = await Api.put(`/admin/items/${itemId}`, postData)  
+}
+
 function matchCategory(categoryId, categoryList)
 {
     return categoryList.filter(x => x._id === categoryId)[0].name
+}
+
+function deMatchCategory(categoryName, categoryList)
+{
+    return categoryList.filter(x => x.name === categoryName)[0]._id
 }
 
 async function Update(event)
@@ -98,7 +141,7 @@ async function Update(event)
     {
         const table = btnTouched.closest("table")
         const currRow = btnTouched.closest("tr")
-        const itemId = idArray[Number(currRow.cells[0].innerHTML) - 1]
+        const itemId = itemArray[0][Number(currRow.cells[0].innerHTML) - 1]
         console.log(itemId)
         //delete Row
         table.deleteRow(currRow.rowIndex)
@@ -115,36 +158,38 @@ async function Update(event)
     }
     if (btnTouched.id === "modify")
     {
+      const currRow = btnTouched.closest("tr")
       const modal = document.querySelector("#modal");
+      populateForm(currRow);
       modal.showModal();
     }
 }
 
-const modalCloseModal = document.querySelector(".close-button");
-modalCloseModal.addEventListener("click", () => {
-  const modal = document.querySelector("#modal");
-  modal.setAttribute("close", "");
 
-  modal.addEventListener(
-    "transitionend",
-    () => {
-      modal.removeAttribute("close");
-      modal.close();
-    },
-    {once: true}
-  );
-});
-
-async function modifyUpdate(event)
+function closeModal()
 {
-    const btnTouched = event.target
-    if (btnTouched.id !== "modify")
-        return
+    const modal = document.querySelector("#modal");
+    modal.setAttribute("close", "");
+  
+    modal.addEventListener(
+      "transitionend",
+      () => {
+        modal.removeAttribute("close");
+        modal.close();
+      },
+      {once: true}
+    );
+}
 
-    const table = btnTouched.closest("table")
-    const currRow = btnTouched.closest("tr")
-    const itemId = idArray[Number(currRow.cells[0].innerHTML) - 1]
-    console.log(itemId)
+async function getCategory() {
+    var retHTML = ``;
+    var categoryId = [];
+    for (let i=0; i<categorylist.length;i++)
+    {
+        retHTML += `<option>${categorylist[i].name}</option>`
+        categoryId.push(categorylist[i]._id)
+    }
+    prodCate.innerHTML = retHTML;
 }
 
 function isLoggedIn()
